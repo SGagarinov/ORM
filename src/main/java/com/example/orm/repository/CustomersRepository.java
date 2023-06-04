b90e4871e65ee0ae05f8ae564b052eaab51db6b8
Abriial <codjumper@mail.ru>
2023-06-04 16:55:12 +0300

    Jpa.Repository.Task-2 (Query)

--- src/main/java/com/example/orm/repository/CustomersRepository.java
+++ src/main/java/com/example/orm/repository/CustomersRepository.java
@@ -3,13 +3,18 @@ package com.example.orm.repository;
 import com.example.orm.entity.Customer;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CustomersRepository extends JpaRepository<Customer, Long> {
 
-    List<Customer> findByCity (String city);
-    List<Customer> findByAgeLessThan(int age, Sort sort);
-    Optional<List<Customer>> findByNameAndSurname(String name, String surname);
+    @Query("select c from Customer c where c.city=:city")
+    List<Customer> findByCity (@Param("city")String city);
+    @Query("select c from Customer c where c.age < :age order by age")
+    List<Customer> findByAgeLessThan(@Param("age")int age, Sort sort);
+    @Query("select c from Customer c where name=:name and surname=:surname")
+    Optional<List<Customer>> findByNameAndSurname(@Param("name")String name, @Param("surname")String surname);
 }
